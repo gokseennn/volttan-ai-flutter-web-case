69c06161af434186203137d04b9598a2a850b349
Ahmet Göksen Akyıldız <ahmet.goksen81@icloud.com>
2025-10-07 18:28:30 +0300

    name changes

--- lib/screens/home_screen/view/home_screen.dart
+++ lib/screens/home_screen/view/home_screen.dart
@@ -5,6 +5,10 @@ class HomeScreen extends StatelessWidget {
   static const String routeName = '/home_screen';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text('Home Screen'),
+      ),
+    );
   }
 }
