64ca35f38de2b0347fd16da7c902e73362809080
Ahmet Göksen Akyıldız <ahmet.goksen81@icloud.com>
2025-10-08 21:28:23 +0300

    home repository init

--- lib/screens/home_screen/controller/home_binding.dart
+++ lib/screens/home_screen/controller/home_binding.dart
@@ -1,11 +1,15 @@
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:voltran_ai_web/screens/home_screen/controller/home_controller.dart';
+import 'package:voltran_ai_web/screens/home_screen/controller/home_repsoitory.dart';
 
 class HomeBinding extends Binding {
   @override
   List<Bind> dependencies() => [
     Bind.lazyPut(
       () => HomeController(),
+    ),
+    Bind.lazyPut(
+      () => HomeRepsoitory(),
     ),
   ];
 }
